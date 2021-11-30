05057324f1523dbb0948984c41a8d5e7fed25699
osmonalievv <osmonalievbekzhann@gmail.com>
2021-11-30 19:54:35 +0600

    commit1

--- app/src/main/java/kg/tsi/geohis/MainActivityGenGeography.kt
+++ app/src/main/java/kg/tsi/geohis/MainActivityGenGeography.kt
@@ -4,16 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivityGenGeography : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_gen_geography)
-        val click = findViewById<Button>(R.id.but_train)
+        val click = findViewById<CardView>(R.id.map_button)
 
 
         click.setOnClickListener {
-            startActivity(Intent(this, MainActivityAncient::class.java))
+            startActivity(Intent(this, MainActivityGeoMap::class.java))
+
         }
     }
 }
--- app/src/main/java/kg/tsi/geohis/MainActivityMain.kt
+++ app/src/main/java/kg/tsi/geohis/MainActivityMain.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +12,22 @@ class MainActivityMain : AppCompatActivity() {
         setContentView(R.layout.activity_main_main)
 
         //body
-        val click = findViewById<Button>(R.id.but_train)
+        val click = findViewById<CardView>(R.id.ancient_his_button)
 
 
         click.setOnClickListener{
             startActivity(Intent(this,MainActivityAncient::class.java))
+
+            val click = findViewById<CardView>(R.id.medival_history_button)
+
+            click.setOnClickListener{
+                startActivity(Intent(this,MainActivityAncient::class.java))
+
+
+
+                }
+
+
 
        }
     }
